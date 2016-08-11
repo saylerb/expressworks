3ece8dfff0782140fe8e29b473a0317d88a793d8
Brian Sayler <sayler.b@gmail.com>
2016-08-10 21:24:18 -0600

    complete param_pam_pam

---
+++ param_pam_pam.js
@@ -0,0 +1,17 @@
+var express = require('express')
+
+var app = express()
+
+
+app.put('/message/:id', function(req, res) {
+
+  var id = req.params.id
+
+  res.end(require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex')
+  )
+})
+
+app.listen(process.argv[2])
