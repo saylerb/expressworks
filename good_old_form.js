cdfb3a4fb7e227644cb9a296aaab56ab388a2c10
Brian Sayler <sayler.b@gmail.com>
2016-08-10 21:10:49 -0600

    complete good old form

---
+++ good_old_form.js
@@ -0,0 +1,17 @@
+var express = require('express')
+
+var app = express()
+
+var bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.get('/form', function(req, res) {
+  res.render('<form><input name="str"/></form>')
+})
+
+app.post('/form', function(req, res) {
+  res.end(req.body.str.split('').reverse().join(''))
+})
+
+app.listen(process.argv[2])
